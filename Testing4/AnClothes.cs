1299485a950fca6da09c8599a6ed8976d3d6708c
Lawrence Molokwu <P2514538@my365.dmu.ac.uk>
2021-02-18 14:20:24 +0000

    created test for each of my attributes

---
+++ Testing4/AnClothes.cs
@@ -0,0 +1,7 @@
+﻿namespace Testing4
+{
+    class AnClothes
+    {
+        public static decimal Price { get; internal set; }
+    }
+}
---
+++ Testing4/clsStock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Testing4
+{
+    class clsStock
+    {
+        public clsStock()
+        {
+        }
+
+        public object Active { get; internal set; }
+        public DateTime DateStock { get; internal set; }
+        public int ClothesNo { get; internal set; }
+        public DateTime DateAdded { get; internal set; }
+        public decimal Price { get; internal set; }
+        public object ClothesColour { get; internal set; }
+        public string ClothesDescription { get; internal set; }
+    }
+}
