1a7fd912ea6a005a8323ce2e2bfa8cd9697afa38
Lawrence Molokwu <P2514538@my365.dmu.ac.uk>
2021-02-23 03:47:36 +0000

    Added Presentation Layer

--- AdminSystem/StockDataEntry.aspx
+++ AdminSystem/StockDataEntry.aspx
@@ -10,6 +10,28 @@
     <form id="form1" runat="server">
         <div>
         </div>
+        <asp:Label ID="iblClothesNo" runat="server" Text="ClothesNo" width="190px"></asp:Label>
+        <asp:TextBox ID="txtClothesNo" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="iblClothesDescription" runat="server" Text="ClothesDescription "></asp:Label>
+            <asp:TextBox ID="txtClothesDescription" runat="server"></asp:TextBox>
+        </p>
+        <asp:Label ID="iblPrice" runat="server" Text="Price" width="190px"></asp:Label>
+        <asp:TextBox ID="txtPrice" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="iblClothesColour" runat="server" Text="ClothesColour" width="190px"></asp:Label>
+            <asp:TextBox ID="txtClothesColour" runat="server"></asp:TextBox>
+        </p>
+        <asp:Label ID="iblDateAdded" runat="server" Text="DateAdded" width="190px"></asp:Label>
+        <asp:TextBox ID="txtDateAdded" runat="server"></asp:TextBox>
+        <p>
+            <asp:CheckBox ID="chkAvailable" runat="server" Text="Available" />
+        </p>
+        <asp:Label ID="iblError" runat="server"></asp:Label>
+        <p>
+            <asp:Button ID="btnOK" runat="server" OnClick="btnOK_Click1" Text="Ok" />
+            <asp:Button ID="btnCancel" runat="server" Text="Cancel" />
+        </p>
     </form>
 </body>
 </html>
--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -4,11 +4,54 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
+    public object txtAvailable { get; private set; }
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create an instance clsStock
+        clsStock AnStock = new clsStock();
+        // capture the ClothesNo
+        AnStock.ClothesNo = txtClothesNo.Text;
+        // store the Stock in the session object
+        Session["AnStock"] = AnStock;
+        //Navigate to the viewer page 
+        Response.Redirect("AddressBookViewer.aspx");
+
+
+
+    }
+
+    protected void btnOK_Click1(object sender, EventArgs e)
+    {
+        //create an instance of the stock class
+        clsStock AnStock = new clsStock();
+        //variable to store the primary key
+        Int32 ClothesNo;
+        //variable to store the result to find operation
+        Boolean Found = false;
+        //get the primary key entered by the user
+        ClothesNo = Convert.ToInt32(txtClothesNo.Text);
+        //find the record   
+        Found = AnStock.Find(ClothesNo);
+        //if found
+        if (Found == true)
+        {
+            //display the values of the properties in the form
+            txtClothesNo.Text = AnStock.ClothesNo.ToString();
+            txtDateAdded.Text = AnStock.DateAdded.ToString();
+            txtPrice.Text = AnStock.Price.ToString();
+            txtClothesDescription.Text = AnStock.ClothesDescription;
+            txtClothesColour.Text = AnStock.ClothesColour.ToString();
+            
+        }
+}
 }
