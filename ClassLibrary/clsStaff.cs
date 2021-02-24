a7663eb62e77a1bf2b015fb2f84cb766ea7c44ba
P2541117 <P2541117@my365.dmu.ac.uk>
2021-02-24 02:28:33 +0000

    Created test for each of my attributes

---
+++ ClassLibrary/clsStaff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStaff
+    {
+        public decimal Salary;
+        public bool StaffID;
+
+        public string StaffPosition { get; set; }
+        public string FirstName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
