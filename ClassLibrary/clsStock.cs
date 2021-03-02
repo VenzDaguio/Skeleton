857b8acd3c1c7b3f0666089a98ab7cc53f431d84
Lawrence Molokwu <P2514538@my365.dmu.ac.uk>
2021-03-02 04:25:13 +0000

    added Find method

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -4,11 +4,29 @@ namespace ClassLibrary
 {
     public class clsStock
     {
+        //private data member for the address no property
+        private Int32 mStockNo;
         public clsStock()
         {
         }
 
-        public string ClothesNo { get; set; }
+
+        //ClothesNo private member variable 
+        private Int32 mClothesNo;
+        //ClothesNo public property
+        public Int32 ClothesNo
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStockNo;
+            }
+           set
+            {
+                // this line of code allows data into the property
+                mStockNo = value;
+            }
+        }
         public string ClothesDescription { get; set; }
         public string Price { get; set; }
         public string DateAdded { get; set; }
@@ -17,7 +35,10 @@ namespace ClassLibrary
 
         public bool Find(int clothesNo)
         {
-            throw new NotImplementedException();
+            //set the private data members to the test data value
+            mStockNo = 12;
+            //always return true
+            return true;
         }
     }
 }
