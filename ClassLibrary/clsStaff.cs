6e35c39c1ba27d08bcf1abc20c39676ebe21144d
P2541117 <P2541117@my365.dmu.ac.uk>
2021-04-17 07:20:25 +0100

    Completed the find method for the middle layer

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -4,12 +4,96 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
-        public decimal Salary;
-        public bool StaffID;
+        public decimal mSalary;
+        public decimal Salary 
+            {
+            get 
+            {
+                return Salary;
+            }
+            set 
+            {
+              Salary = value;  
+            }
+        }
+        public bool mStaffID;
+        public bool StaffID 
+        {
+            get 
+           {
+            return mStaffID;
+           }
+            set 
+           {
+            StaffID = value;
+           }
+            }
 
-        public string StaffPosition { get; set; }
-        public string FirstName { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public DateTime StartDate { get; set; }
-    }
+        public string mStaffPosition;
+        public string StaffPosition
+        {
+        get
+        {
+          return mStaffPosition; 
+        } 
+        set
+        {
+          mStaffPosition = value; 
+        }
 }
+
+        public string mFirstName;
+        public string FirstName
+        { 
+         get
+         {
+           return mFirstName;
+
+         }
+           set
+            {
+              FirstName = value; 
+           }
+        }
+
+
+        public DateTime mDateOfBirth;
+        public DateTime DateOfBirth 
+        { 
+        get 
+        {
+          return DateOfBirth;
+        }
+        set
+        {
+          DateOfBirth = value;  
+        }
+     }
+
+        public DateTime mStartDate; 
+        public DateTime StartDate 
+        { 
+        get
+        {
+          return StartDate;
+        }
+        set
+        {
+         StartDate = value; 
+        }
+    }
+
+        public bool Find(decimal Salary )
+        {
+            //set the private data members to the test data value
+            mSalary = 1000;
+            mStaffID = "123";
+            mStaffPosition = "Member";
+            mFirstName= "Kiana";
+            mDateOfBirth = Convert.ToDateTime("26/05/2015");
+            mStartDate = Convert.ToDateTime("26/05/2015");
+            //always return true
+            return true;
+}
+        }
+    }
--- Testing2/tstStaff.cs
+++ Testing2/tstStaff.cs
@@ -97,7 +97,159 @@ namespace Testing2
             Assert.AreEqual(AnStaff.StaffPosition, TestData);
         }
 
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //create some test data to use with the method
+            Int32 StaffID = 1;
+            //invoke the method
+            Found = AnStaff.Find(StaffID);
+            //test to see that the result is correct
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void StaffIDFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 1;
+            //invoke the method
+            Found = AnStaff.Find(StaffID);
+            //check the address no
+            if (AnStaff.StaffID != 1)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void DateOfBirthFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 DateOfBirth = 21;
+            //invoke the method
+            Found = AnStaff.Find(DateOfBirth);
+            //check the property
+            if (AnStaff.StartDate != Convert.ToDateTime("16/09/2015"))
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void SalaryFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Decimal Salary = 21;
+            //invoke the method
+            Found = AnStaff.Find(Salary);
+            //check the property
+            if (AnStaff.Salary != 1000)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        
+
+        [TestMethod]
+        public void FirstNameFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID = 21;
+            //invoke the method
+            Found = AnStaff.Find(FirstName);
+            //check the property
+            if (AnStaff.FirstName != "Kiana")
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void StartDateFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StartDate = 21;
+            //invoke the method
+            Found = AnStaff.Find(StartDate);
+            //check the property
+            if (AnStaff.StartDate != Convert.ToDateTime("16/09/2015"))
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+        [TestMethod]
+        public void StaffPositionFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff AnStaff = new clsStaff();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            Int32 StaffID  = 21;
+            //invoke the method
+            Found = AnStaff.Find(StaffPosition);
+            //check the property
+            if (AnStaff.StaffPosition != true)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+
     }
 }
 
-       
+
+
