762b1b2eeccb7d768e5b290a75c450fb8c5e5b00
Paolo Daguio <paolodaguio@gmail.com>
2021-04-19 16:03:22 +0100

    Added filter and delete method

--- ClassLibrary/clsOrderCollection.cs
+++ ClassLibrary/clsOrderCollection.cs
@@ -8,6 +8,12 @@ namespace ClassLibrary
         List<clsOrder> mOrderList = new List<clsOrder>();
         clsOrder mThisOrder = new clsOrder();
 
+        public clsOrderCollection()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblOrder_SelectAll");
+            PopulateArray(DB);
+        }
         public List<clsOrder> OrderList
         {
             get
@@ -67,6 +73,43 @@ namespace ClassLibrary
             DB.AddParameter("@DateReceived", mThisOrder.DateReceived);
 
             DB.Execute("sproc_tblOrder_Update");
+        }
+
+        public void Delete()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@OrderId", mThisOrder.OrderId);
+            DB.Execute("sproc_tblOrder_Delete");
+        }
+
+        public void ReportByDescription(string Description)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@Description", Description);
+            DB.Execute("sproc_TblOrder_FilterByDescription");
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            Int32 Index = 0;
+            Int32 RecordCount;
+            RecordCount = DB.Count;
+            mOrderList = new List<clsOrder>();
+            while (Index < RecordCount)
+            {
+                clsOrder AnOrder = new clsOrder();
+                AnOrder.Delivered = Convert.ToBoolean(DB.DataTable.Rows[Index]["Delivered"]);
+                AnOrder.OrderId = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderId"]);
+                AnOrder.CustomerId = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerId"]);
+                AnOrder.OrderPrice = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderPrice"]);
+                AnOrder.Description = Convert.ToString(DB.DataTable.Rows[Index]["Description"]);
+                AnOrder.OrderLineId = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderLineId"]);
+                AnOrder.Quantity = Convert.ToInt32(DB.DataTable.Rows[Index]["Quantity"]);
+                AnOrder.DateReceived = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateReceived"]);
+                mOrderList.Add(AnOrder);
+                Index++;
+            }
         }
     }
 }
