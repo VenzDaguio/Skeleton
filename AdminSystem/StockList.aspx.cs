98b299de759c1c4a0efbd6133b2695eafd818df8
Lawrence Molokwu <P2514538@my365.dmu.ac.uk>
2021-04-19 19:03:08 +0100

    Added presentation layer fpr collection class

--- AdminSystem/StockList.aspx.cs
+++ AdminSystem/StockList.aspx.cs
@@ -4,11 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
-public partial class _1_List : System.Web.UI.Page
+public partial class _Default : System.Web.UI.Page
 {
+    //this function handles the load event 
     protected void Page_Load(object sender, EventArgs e)
     {
+        
+        //if this is the first time the page is dsiplayed
+        if (IsPostBack == false)
+        {
+           
+            
+                //update the list box
+                DisplayStocks();
+
+            
+            
+        }
 
     }
+    void DisplayStocks()
+    {
+        clsStockCollection Stocks = new clsStockCollection();
+        lstStocks.DataSource = Stocks.StockList;
+        lstStocks.DataValueField = "ClothesNo";
+        lstStocks.DataTextField = "ClothesDescription";
+        lstStocks.DataBind();
+
+    }
+
 }
